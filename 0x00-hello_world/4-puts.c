261a0537666d408364b80825cb553d4e2d918209
GoodnessOseghale <goodnessoseghalefeelz@gmail.com>
2023-06-17 16:35:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*This is a task for alx.
+*
+*Main: this function is the entry point.
+*
+*Return: this should always be 0 (successs).
+*
+*Description: Hello c for printing.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
