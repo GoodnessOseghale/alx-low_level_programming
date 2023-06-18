58f97e7cdbcf38df121b8658189adc15d7665490
GoodnessOseghale <goodnessoseghalefeelz@gmail.com>
2023-06-17 17:05:05 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+* main -  This function is the entry point.
+*
+* Return: This should always be 0 (successs).
+*
+* Description: Hello c for printing.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
