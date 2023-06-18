7c12fd55f6339f7eb378f149cf2f5a3dd529b0a9
GoodnessOseghale <goodnessoseghalefeelz@gmail.com>
2023-06-17 20:05:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,19 +3,12 @@
 /**
 * main -  This function is the entry point.
 *
-* Return: This should always be 0 (successs).
+* Return: This should always be 1 (successs).
 *
 * Description: Hello c for printing.
 */
 int main(void)
 {
-	FILE *fp;
-	char str[] = "and that piece of art is useful - Dora Korpar, 2015-10-19";
-
-	fp = fopen("file.text", "w");
-	fwrite(str, 1, sizeof(str), fp);
-
-	fclose(fp);
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
