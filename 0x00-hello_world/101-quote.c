6fbd9626ec666e3585a26b24e67b37abe328d10b
GoodnessOseghale <goodnessoseghalefeelz@gmail.com>
2023-06-17 18:36:42 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+* main -  This function is the entry point.
+*
+* Return: This should always be 0 (successs).
+*
+* Description: Hello c for printing.
+*/
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
