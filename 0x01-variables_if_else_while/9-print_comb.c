74161227e15a7dadf199e6326bfeb3574c8a7538
GoodnessOseghale <goodnessoseghalefeelz@gmail.com>
2023-06-19 12:46:12 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main -  This function is the entry point.
+* Return: This should always be 0 (successs).
+* Description: Numbers with commas and space for printing.
+*/
+
+int main(void)
+{
+	 int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
